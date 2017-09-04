dbbb57639adc9e26187db0cbf8ee7c5df43762ad
motecshine <shinemotec@gmail.com>
2017-09-04 15:11:19 +0800

    initial repo

--- container.c
+++ container.c
@@ -27,63 +27,6 @@
 #include "ext/standard/info.h"
 #include "php_container.h"
 
-/* If you declare any globals in php_container.h uncomment this:
-ZEND_DECLARE_MODULE_GLOBALS(container)
-*/
-
-/* True global resources - no need for thread safety here */
-static int le_container;
-
-/* {{{ PHP_INI
- */
-/* Remove comments and fill if you need to have entries in php.ini
-PHP_INI_BEGIN()
-    STD_PHP_INI_ENTRY("container.global_value",      "42", PHP_INI_ALL, OnUpdateLong, global_value, zend_container_globals, container_globals)
-    STD_PHP_INI_ENTRY("container.global_string", "foobar", PHP_INI_ALL, OnUpdateString, global_string, zend_container_globals, container_globals)
-PHP_INI_END()
-*/
-/* }}} */
-
-/* Remove the following function when you have successfully modified config.m4
-   so that your module can be compiled into PHP, it exists only for testing
-   purposes. */
-
-/* Every user-visible function in PHP should document itself in the source */
-/* {{{ proto string confirm_container_compiled(string arg)
-   Return a string to confirm that the module is compiled in */
-PHP_FUNCTION(confirm_container_compiled)
-{
-	char *arg = NULL;
-	size_t arg_len, len;
-	zend_string *strg;
-
-	if (zend_parse_parameters(ZEND_NUM_ARGS(), "s", &arg, &arg_len) == FAILURE) {
-		return;
-	}
-
-	strg = strpprintf(0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "container", arg);
-
-	RETURN_STR(strg);
-}
-/* }}} */
-/* The previous line is meant for vim and emacs, so it can correctly fold and
-   unfold functions in source code. See the corresponding marks just before
-   function definition, where the functions purpose is also documented. Please
-   follow this convention for the convenience of others editing your code.
-*/
-
-
-/* {{{ php_container_init_globals
- */
-/* Uncomment this function if you have INI entries
-static void php_container_init_globals(zend_container_globals *container_globals)
-{
-	container_globals->global_value = 0;
-	container_globals->global_string = NULL;
-}
-*/
-/* }}} */
-
 /* {{{ PHP_MINIT_FUNCTION
  */
 PHP_MINIT_FUNCTION(container)
@@ -146,7 +89,6 @@ PHP_MINFO_FUNCTION(container)
  * Every user visible function must have an entry in container_functions[].
  */
 const zend_function_entry container_functions[] = {
-	PHP_FE(confirm_container_compiled,	NULL)		/* For testing, remove later. */
 	PHP_FE_END	/* Must be the last line in container_functions[] */
 };
 /* }}} */
