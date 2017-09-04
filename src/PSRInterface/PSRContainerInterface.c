5e0d38630a5cf3051c35b4f591c2a2bba670007a
motecshine <shinemotec@gmail.com>
2017-09-04 17:10:10 +0800

    finished psr interface

---
+++ src/PSRInterface/PSRContainerInterface.c
@@ -0,0 +1,37 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+
+/* include global headers*/
+#include "php_container.h"
+#include "src/PSRInterface/PSRContainerInterface.h"
+
+zend_class_entry *PSRContainerInferface;
+
+ZEND_BEGIN_ARG_INFO(getArgInfo, 0)
+ZEND_ARG_INFO(0, id)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(hasArgInfo, 0)
+ZEND_ARG_INFO(0, id)
+ZEND_END_ARG_INFO()
+
+zend_function_entry PSRContainerInterfaceMethods[] = {
+    ZEND_ABSTRACT_ME(PSRContainerInferface, get, getArgInfo)
+    ZEND_ABSTRACT_ME(PSRContainerInferface, has, hasArgInfo)
+    PHP_FE_END
+};
+
+PHP_CONTAINER_STARTUP_FUNCTION(PSRContainerInferface)
+{
+    zend_class_entry PSRContainerInferfaceTempCE;
+    INIT_CLASS_ENTRY(PSRContainerInferfaceTempCE, "PSR\\ContainerInterface", PSRContainerInterfaceMethods);
+    PSRContainerInferface = zend_register_internal_class(&PSRContainerInferfaceTempCE TSRMLS_CC);
+    if (PSRContainerInferface == NULL) {
+        return FAILURE;
+    }
+    PSRContainerInferface->ce_flags = ZEND_ACC_INTERFACE;
+    return SUCCESS;
+}
